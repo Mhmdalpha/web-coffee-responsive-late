9a945b239fd6988ad12173c167b8c00940601dba
Muhammad Alpha <122379779+Mhmdalpha@users.noreply.github.com>
2023-12-11 12:03:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -28,5 +28,29 @@ window.onscroll = () => {
     cartItem.classList.remove('active');
 }
 
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_tfkczaj";
+    const templateID = "template_5n031pc";
+    const publicKey = "YqpFfryCSeO02WBgw";
+    
+    emailjs.send(serviceID,templateID,params,publicKey).then((res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("phone").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            
+            alert("your message sent succesfully");
+        }
+    ).cath((err) => console.log(err))
+}
+
 
 
